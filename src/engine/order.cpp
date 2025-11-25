7cb02c409b97cb5be0e0c6b5089b1efd65493ad3
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2025-11-25 15:00:59 -0500

    Begin implementation of order

--- src/engine/order.cpp
+++ src/engine/order.cpp
@@ -3,3 +3,17 @@
 
 
 */
+
+#include <bool>
+#include <string>
+#define BUY 0
+#define SELL 1
+
+struct order
+{
+	unint64 order_id;
+	std::string symbol;
+	std::bool side;
+	int64 ticks price;	
+}
+
