1481c2da27dc1576a2b6b99c81edeb0a8bc88d99
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2025-12-14 00:55:34 -0500

    Implement private member variables of OrderBook

--- src/engine/book.h
+++ src/engine/book.h
@@ -3,12 +3,22 @@
 */
 
 #include <deque>
+#include <functional>
 #include <map>
+#include <memory>
+#include "order.h"
+
+struct PriceLevel
+{
+	int64_t price;
+	std::deque<Order*> orders;	
+};
 
 class OrderBook
 {
-
-	std::map<int64_t price, std::deque<order>
+	private:
+		std::map<int64_t price, std::unique_ptr<PriceLevel>, std::greater<>> bids;
+		std::map<int64_t price, std::unique_ptr<PriceLevel>, std::less<>> asks;
 
 }
 
