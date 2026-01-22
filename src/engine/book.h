aad195b83bb402ef6f75c83eaa8af0a62d0b5986
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2026-01-21 21:04:33 -0500

    Implement SymbolId sruct

--- src/engine/book.h
+++ src/engine/book.h
@@ -14,11 +14,31 @@ struct PriceLevel
 	std::deque<Order*> orders;	
 };
 
+stuct SymbolId
+{
+	uint64_t value;
+
+	explicit SymbolId(uint64_t v) : value(v) {}
+
+	bool operator==(const SymbolId& other) const
+	{
+		return this.value == other.value;
+	}
+	
+	bool operator!=(const SymbolId& other) const
+	{
+		return this.value != other.value;
+	}
+}
+
 class OrderBook
 {
 	private:
+		SymbolId symbol;
 		std::map<int64_t price, std::unique_ptr<PriceLevel>, std::greater<>> bids;
 		std::map<int64_t price, std::unique_ptr<PriceLevel>, std::less<>> asks;
 
+	public:
+		explicit OrderBook(SymbolId s) noexcept : symbol(s) {} 
 }
 
