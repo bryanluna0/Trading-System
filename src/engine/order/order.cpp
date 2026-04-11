0e1a97ae5d81b67e4984859a71e9f958e688d0db
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2026-04-11 01:54:58 -0400

    Implement full order class

--- src/engine/order/order.cpp
+++ src/engine/order/order.cpp
@@ -5,7 +5,26 @@
 
 #include "order.h"
 
-bool Order::is_valid() const noexcept
+std::atomic<uint64_t> Order::next_id = 0;
+
+Order::Order(SymbolId symbol_id, int64_t price, int64_t quantity, OrderType order_type, Side side) : 
+		symbol_id(symbol_id)
+		,order_id(OrderId(next_id.fetch_add(1)))
+		,price(price)
+		,quantity(quantity)
+		// get time stamp in ns
+		,timestamp((std::chrono::nanoseconds(std::chrono::steady_clock::now().time_since_epoch())).count())
+		,order_type(order_type), side(side) 
+{}
+
+std::optional<Order> Order::createOrder(SymbolId symbol_id, int64_t price, int64_t quantity, OrderType order_type, Side side)
 {
-	return price > 0 && quantity > 0 && timestamp >= 0;
+	// TODO: check that symbol id is valid
+	// TODO: return error code based on error
+	if (price <= 0 || quantity <= 0)
+	{
+		return {};
+	}
+
+	return Order(symbol_id, price, quantity, order_type, side);
 }
--- src/engine/order/order.h
+++ src/engine/order/order.h
@@ -6,22 +6,32 @@
 #ifndef ORDER_H
 #define ORDER_H
 
+#include <atomic>
+#include <chrono>
+#include <optional>
 #include <string>
 
 #include "types/types.h"
 
 // TODO: ensure all variables are instantiated before they are used
-struct Order
+class Order
 {
-	SymbolId symbol_id;
-	OrderId order_id;
+	friend class OrderBook;
+private:
 	int64_t price;	
 	int64_t quantity;
-	int64_t timestamp;	
-	OrderType order_type;
-	Side side;
 
-	bool is_valid() const noexcept;
+	static std::atomic<uint64_t	> next_id;
+	
+	Order(SymbolId symbol_id, int64_t price, int64_t quantity, OrderType order_type, Side side);
+public:
+	const SymbolId symbol_id;
+	const OrderId order_id;
+	const int64_t timestamp;
+	const OrderType order_type;
+	const Side side;
+
+	std::optional<Order> createOrder(SymbolId symbol_id, int64_t price, int64_t quantity, OrderType order_type, Side side);
 };
 
 #endif // ORDER_H
